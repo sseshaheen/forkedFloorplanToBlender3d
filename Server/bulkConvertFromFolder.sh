5a6aa55b1342d466fa522a467d736c7a53bbbb90
shaheer <shaheereshaheen@gmail.com>
2024-05-31 02:08:09 +0300

    .

--- Server/bulkConvertFromFolder.sh
+++ Server/bulkConvertFromFolder.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Enable debugging
+set -x
+
 # Check if the directory is provided
 if [ -z "$1" ]; then
     echo "Usage: $0 <directory>"
@@ -10,6 +13,9 @@ IMAGE_DIR="$1"
 OBJECT_DIR="./storage/objects"
 BOUNDARY="----WebKitFormBoundary7MA4YWxkTrZu0gW"
 SUPPORTED_FORMATS="jpg jpeg png tiff bmp gif"
+
+echo "Starting script to process images in directory: $IMAGE_DIR"
+echo "Supported formats: $SUPPORTED_FORMATS"
 
 # Loop through each file in the directory
 for file in "$IMAGE_DIR"/*; do
@@ -25,9 +31,13 @@ for file in "$IMAGE_DIR"/*; do
                           -H "Accept: application/json" \
                           -F "file=@${file}")
 
+        echo "POST response: $response"
+
         # Parse the response
         id=$(echo "$response" | awk -F\' '{print $2}')
         hash=$(echo "$response" | awk -F\' '{print $4}')
+
+        echo "Parsed id: $id, hash: $hash"
 
         # File name without directory
         filename=$(basename -- "$file")
@@ -38,14 +48,18 @@ for file in "$IMAGE_DIR"/*; do
                           -H "Content-Type: multipart/form-data; boundary=$BOUNDARY" \
                           --form "file=@${file}")
 
+        echo "PUT response: $put_response"
+
         # Check if the .obj file is created every 2 seconds
         obj_file="${OBJECT_DIR}/${id}.obj"
         while [ ! -f "$obj_file" ]; do
+            echo "Waiting for $obj_file to be created..."
             sleep 2
         done
 
-        # Series of actions
         echo "$obj_file has been created."
+
+        # Series of actions
         # Add your series of actions here
 
     else
