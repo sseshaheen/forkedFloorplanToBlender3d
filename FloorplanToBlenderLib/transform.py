43f4d60cfd1fe21fae6b2b4f56c959cd17e02c56
shaheer <shaheereshaheen@gmail.com>
2024-05-31 00:27:10 +0300

    .

--- FloorplanToBlenderLib/transform.py
+++ FloorplanToBlenderLib/transform.py
@@ -54,8 +54,8 @@ def save_debug_info(filename, data):
         filepath = os.path.join(DEBUG_STORAGE_PATH, filename)
         with open(filepath, 'a') as file:
             file.write(str(data))
-        if LOGGING_VERBOSE:
-            logger.debug(f'Saved debug info: {filepath}')
+        # if LOGGING_VERBOSE:
+        #     logger.debug(f'Saved debug info: {filepath}')
 
 def rescale_rect(list_of_rects, scale_factor):
     """
