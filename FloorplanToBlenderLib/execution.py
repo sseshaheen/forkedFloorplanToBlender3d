77be147902ca399fce1fb709fc8892df3baa5d17
shaheer <shaheereshaheen@gmail.com>
2024-05-30 23:26:43 +0300

    .

--- FloorplanToBlenderLib/execution.py
+++ FloorplanToBlenderLib/execution.py
@@ -17,9 +17,9 @@ Copyright (C) 2022 Daniel Westberg
 # Configure logging
 if LOGGING_VERBOSE:
     # Load the DEBUG_SESSION_ID from the JSON file
-    config = load_config_from_json('./config.json')
+    debug_config = load_config_from_json('./config.json')
     
-    log_file_path = os.path.join('./storage/debug', config['DEBUG_SESSION_ID'])
+    log_file_path = os.path.join('./storage/debug', debug_config['DEBUG_SESSION_ID'])
     os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 
 
