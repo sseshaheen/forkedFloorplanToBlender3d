da3849346b69bfacb3eeef54b997da86e33c79c4
shaheer <shaheereshaheen@gmail.com>
2024-05-31 00:40:38 +0300

    .

--- FloorplanToBlenderLib/calculate.py
+++ FloorplanToBlenderLib/calculate.py
@@ -31,12 +31,19 @@ if LOGGING_VERBOSE:
     file_handler = logging.FileHandler(log_file_path)
     file_handler.setLevel(logging.DEBUG)
 
-    # Create formatter and add it to the handler
+    # Create a console handler to log warnings and above
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.WARNING)
+
+    # Create formatters and add them to the handlers
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
 
-    # Add the file handler to the logger
+    # Add handlers to the logger
     logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
 
 
 
