226ea23f51649fcfddbed01bd2b87d4f8b10d624
shaheer <shaheereshaheen@gmail.com>
2024-06-02 12:04:48 +0300

    create wrapper shell script for process_pending_jobs_to_firebase.py

---
+++ Server/run_process_pending_jobs_to_firebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /home/apps/forkedFloorplanToBlender3d/myenv/bin/activate
+
+# Run the Python script
+python /home/apps/forkedFloorplanToBlender3d/process_pending_jobs.py
