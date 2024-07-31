b308fd41122b51cd09044b03845c69801f789209
shaheer <shaheereshaheen@gmail.com>
2024-07-31 22:26:11 +0300

    edit blender_script.py

--- Server/blender_script.py
+++ Server/blender_script.py
@@ -88,8 +88,8 @@ def main():
 
     # create_floorplan(base_path, program_path)
     create_walls(base_path, program_path)
-    create_doors(base_path, program_path)
-    create_windows(base_path, program_path)
+    # create_doors(base_path, program_path)
+    # create_windows(base_path, program_path)
     # create_others(base_path, program_path)
 
     bpy.ops.wm.save_as_mainfile(filepath=os.path.join(base_path, "floorplan.blend"))
