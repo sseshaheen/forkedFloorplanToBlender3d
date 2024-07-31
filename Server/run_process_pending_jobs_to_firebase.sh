21611ca67a78565fb71fc40178f78b2b6c31c07c
shaheer <shaheereshaheen@gmail.com>
2024-07-31 19:42:54 +0300

    .

--- Server/run_process_pending_jobs_to_firebase.sh
+++ Server/run_process_pending_jobs_to_firebase.sh
@@ -3,8 +3,5 @@
 # Activate the virtual environment
 source /home/apps/forkedFloorplanToBlender3d/myenv/bin/activate
 
-# Define the log file
-LOGFILE="/home/apps/forkedFloorplanToBlender3d/logs/process_pending_jobs.log"
-
-# Run the Python script and redirect stdout and stderr to the log file
-python /home/apps/forkedFloorplanToBlender3d/Server/process_pending_jobs_to_firebase.py >> $LOGFILE 2>&1
+# Run the Python script
+python /home/apps/forkedFloorplanToBlender3d/Server/process_pending_jobs_to_firebase.py
