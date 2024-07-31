9881d6b8e56f7fdb3008fca5e8529cb6cbf0c4cd
shaheer <shaheereshaheen@gmail.com>
2024-07-31 19:27:03 +0300

    .

--- Server/process_pending_jobs_to_firebase.py
+++ Server/process_pending_jobs_to_firebase.py
@@ -475,7 +475,7 @@ if __name__ == "__main__":
 
 
         if os.path.isdir(job_folder) and os.path.exists(job_file_path):
-            logging.info(f"Processing job: {job_id}")
+            # logging.info(f"Processing job: {job_id}")
             with open(job_file_path, "r") as job_file:
                 job_data = json.load(job_file)
 
